6b5cecc97efc9f5cbdaef4d8fecd0e1eb7c67cb9
dbo99 <oconnor.david.b@gmail.com>
2023-02-22 11:31:06 -0800

    Add files via upload

--- cslough/dfbuild.r
+++ cslough/dfbuild.r
@@ -8,7 +8,7 @@ rm(list = ls())
   library(ggridges)
   library(viridis)
   library(zoo)
-  library(plotly)
+  library(ggplotly)
   rstudioapi::getActiveDocumentContext
   setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) }
 
@@ -19,18 +19,18 @@ rm(list = ls())
 {
 piezpos <- read_csv("piezpos.csv")
 head(piezpos)
-cs1 <- read_csv("r1.csv")     %>% mutate(piezo_nn = "cs1")
-cs2 <- read_csv("r2.csv")     %>% mutate(piezo_nn = "cs2")
-cs3 <- read_csv("r3.csv")     %>% mutate(piezo_nn = "cs3")
-cs4 <- read_csv("r4.csv")     %>% mutate(piezo_nn = "cs4")
-cs5 <- read_csv("r5.csv")     %>% mutate(piezo_nn = "cs5")
-cs6 <- read_csv("r6.csv")     %>% mutate(piezo_nn = "cs6")
-cs7 <- read_csv("r7.csv")     %>% mutate(piezo_nn = "cs7")
-cs8 <- read_csv("r8.csv")     %>% mutate(piezo_nn = "cs8")
-cs9 <- read_csv("r9.csv")     %>% mutate(piezo_nn = "cs9")
-cs10 <- read_csv("r10.csv")   %>% mutate(piezo_nn = "cs10")
-cs11 <- read_csv("r11.csv")   %>% mutate(piezo_nn = "cs11")
-csdvj <- read_csv("rDVJ.csv") %>% mutate(piezo_nn = "csdvj")
+cs1 <- read_csv("r1.csv")     %>% mutate(piezo_nn = "cs1"  )
+cs2 <- read_csv("r2.csv")     %>% mutate(piezo_nn = "cs2"  )
+cs3 <- read_csv("r3.csv")     %>% mutate(piezo_nn = "cs3"  )
+cs4 <- read_csv("r4.csv")     %>% mutate(piezo_nn = "cs4"  )
+cs5 <- read_csv("r5.csv")     %>% mutate(piezo_nn = "cs5"  )
+cs6 <- read_csv("r6.csv")     %>% mutate(piezo_nn = "cs6"  )
+cs7 <- read_csv("r7.csv")     %>% mutate(piezo_nn = "cs7"  )
+cs8 <- read_csv("r8.csv")     %>% mutate(piezo_nn = "cs8"  )
+cs9 <- read_csv("r9.csv")     %>% mutate(piezo_nn = "cs9"  )
+cs10 <- read_csv("r10.csv")   %>% mutate(piezo_nn = "cs10" )
+cs11 <- read_csv("r11.csv")   %>% mutate(piezo_nn = "cs11" )
+csdvj <- read_csv("rDVJ.csv") %>% mutate(piezo_nn = "dvj")
 
 pz_ts <- rbind(cs1, cs2, cs3, cs4, cs5, cs6, cs7, cs8, cs9, cs10, cs11,  csdvj)
 head(pz_ts)
@@ -42,3 +42,19 @@ pz_ts <- pz_ts %>% mutate(date = mdy(date), date_tm = paste0(date, " ", time))
 head(pz_ts)
 pz_ts <- pz_ts %>% mutate(date_tm = ymd_hms(date_tm))
 head(pz_ts)
+
+pz_pos_ts <- inner_join(piezpos, pz_ts)
+head(pz_pos_ts)
+
+pz_pos_ts <- pz_pos_ts %>% mutate(ft_bgs = depblotoc - stickup, elev1 = gelev1 + stickup - depblotoc,
+                                  elev2 = gelev2 + stickup - depblotoc, year = year(date), yday = yday(date),
+                                  bl_depchange = depblotoc - bl_dpblotoc)
+head(pz_pos_ts)
+
+p1 <- ggplot(pz_pos_ts, aes(x = date, y = bl_depchange, color = piezo_nn)) + geom_line() #+ facet_wrap(~piezo_nn, ncol = 1)
+p1
+ggsave("explore1.pdf", dpi = 300, width = 25, height = 12, units = "in") 
+
+p1ly <- plotly()
+
+
