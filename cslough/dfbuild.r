8e9a4e30e360423ef27386eceeb9002e9b04114c
dbo99 <oconnor.david.b@gmail.com>
2023-02-22 18:06:24 -0800

    Add files via upload

--- cslough/dfbuild.r
+++ cslough/dfbuild.r
@@ -50,8 +50,29 @@ pz_pos_ts <- pz_pos_ts %>% mutate(ft_bgs = depblotoc - stickup, elev1 = gelev1 +
                                   elev2 = gelev2 + stickup - depblotoc, year = year(date), yday = yday(date),
                                   bl_depchange = bl_dpblotoc - depblotoc, 
                                   feb_bl_depchange = feb_bl_dpblotoc - depblotoc,
-                                  decimal_date = decimal_date(date))
+                                  decimal_date = decimal_date(date), month = month(date))
 head(pz_pos_ts)
+
+
+mon_name <- c("jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec")
+month <- seq(from =1, to = 12, by = 1)
+montable <- data.frame(mon_name, month)
+ 
+
+pz_pos_ts <- inner_join( pz_pos_ts, montable)
+
+pz_pos_ts$mon_name <- factor(pz_pos_ts$mon_name, levels = 
+                               c( "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec", "jan"))
+
+pz_pos_ts$piezo_nn <- factor(pz_pos_ts$piezo_nn, levels = 
+                               c( "cs1", "cs2", "cs3", "cs4", "cs5", "cs6", "cs7", "cs8", "cs9", "cs10", "cs11",  "dvj"))
+
+
+
+
+#################################
+############## plots
+#################################
 xscale_breaks = c( 2011.085, 2011.581, 
                    2012.085, 2012.581, 
                    2013.085, 2013.581,
@@ -82,16 +103,45 @@ xscale_labels = c( "2/1/2011", "8/1/2011",
                   
 #decimal_date(mdy("2/1/2011")) - make start and end months of timeseries similar..last measurement was feb
 
-p1 <- ggplot(pz_pos_ts %>% filter(decimal_date > 2011.085), aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+feb_bl <- ggplot(pz_pos_ts %>% filter(decimal_date > 2011.085), aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
       #ggplot(pz_pos_ts , aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
   geom_hline(yintercept = 0, color = "black") + scale_x_continuous(breaks = xscale_breaks, labels = xscale_labels) +   geom_hline(yintercept = 0, color = "black") + 
               geom_smooth(method = lm, se = FALSE, size = .5) + 
               geom_vline(xintercept = 2011.085, color = "red") +
               geom_vline(xintercept = 2023.085, color = "red") #+ theme(axis.text.x = element_text(angle = 90))
-p1
-ggsave("explore3.pdf", dpi = 300, width = 25, height = 12, units = "in") 
+feb_bl
+ggsave("feb11baseline.pdf", dpi = 300, width = 25, height = 12, units = "in") 
 
-p1ly <- plotly()
+wtabl <- ggplot(pz_pos_ts, aes(x = decimal_date, y = elev2, color = piezo_nn)) + geom_point() + 
+  #facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+  #ggplot(pz_pos_ts , aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+  scale_x_continuous(breaks = xscale_breaks, labels = xscale_labels) +   
+  #geom_smooth(method = lm, se = FALSE, size = .5) + 
+  geom_vline(xintercept = 2011.085, color = "red") + geom_line(size = 0.25) +
+  geom_vline(xintercept = 2023.085, color = "red") #+ theme(axis.text.x = element_text(angle = 90))
+wtabl
+ggsave("watertable.pdf", dpi = 300, width = 25, height = 12, units = "in") 
 
-decimal_date(mdy("2/1/2011"))
 
+ft_bgs_free <- ggplot(pz_pos_ts, aes(x = decimal_date, y = ft_bgs*(-1), color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+  #ggplot(pz_pos_ts , aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+  geom_hline(yintercept = 0, color = "black") + scale_x_continuous(breaks = xscale_breaks, labels = xscale_labels) +   geom_hline(yintercept = 0, color = "black") + 
+  #geom_smooth(method = lm, se = FALSE, size = .5) + 
+  geom_vline(xintercept = 2011.085, color = "red") +
+  geom_vline(xintercept = 2023.085, color = "red") #+ theme(axis.text.x = element_text(angle = 90))
+ft_bgs_free
+ggsave("ft_bgs_free.pdf", dpi = 300, width = 25, height = 12, units = "in") 
+
+ft_bgs <- ggplot(pz_pos_ts, aes(x = decimal_date, y = ft_bgs*(-1), color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1) +
+  #ggplot(pz_pos_ts , aes(x = decimal_date, y = feb_bl_depchange, color = piezo_nn)) + geom_point() + facet_wrap(~piezo_nn, ncol = 1, scales = "free_y") +
+  geom_hline(yintercept = 0, color = "black") + scale_x_continuous(breaks = xscale_breaks, labels = xscale_labels) +   geom_hline(yintercept = 0, color = "black") + 
+  #geom_smooth(method = lm, se = FALSE, size = .5) + 
+  geom_vline(xintercept = 2011.085, color = "red") +
+  geom_vline(xintercept = 2023.085, color = "red") #+ theme(axis.text.x = element_text(angle = 90))
+ft_bgs
+ggsave("ft_bgs.pdf", dpi = 300, width = 25, height = 12, units = "in") 
+
+
+monthboxes <- ggplot(pz_pos_ts, aes(mon_name, y = ft_bgs*(-1), group = month, color = piezo_nn)) + geom_boxplot() + facet_wrap(~piezo_nn)
+monthboxes
+ggsave("mon_boxes.pdf", dpi = 300, width = 25, height = 12, units = "in") 
