39e4d502f2d7367d46ec21420506285f7dadec00
dbo99 <oconnor.david.b@gmail.com>
2024-02-08 23:44:08 -0800

    Add files via upload

---
+++ lar/fairoaks.r
@@ -0,0 +1,56 @@
+
+
+
+
+rm(list = ls())
+
+rstudioapi::getActiveDocumentContext
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("fun_defs.r")
+
+#11446500 #fair oaks
+
+siteNo <- "11446500"
+pCode <- "00060" 
+start.date <- "1987-10-01"
+end.date <- "2023-09-30"
+
+
+amrcn_fo <- readNWISuv(siteNumbers = siteNo,
+                       parameterCd = pCode,
+                       startDate = start.date,
+                       endDate = end.date)
+
+amrcn_fo <- renameNWISColumns(amrcn_fo)
+names(amrcn_fo)
+
+df <- amrcn_fo %>% mutate(wy= water_year(dateTime))
+head(df)
+
+df_annpeak <- df %>% filter(wy != 2024) %>% group_by(wy) %>% summarize(peak = max(Flow_Inst)) #%>% arrange(desc(peak))
+df_annpeak
+#write_csv(df_annpeak, "amrcn_fairoaks_wy_instantaneous_peakflow.txt")
+ggplot(df_annpeak, aes(wy, peak)) + geom_bar(stat = "identity") +
+  scale_x_continuous(breaks = c(1987, 1997, 2006, 2011, 2017, 2023)) + 
+  labs(x = "water year", y = "peak instantaneous flow rate") + gghighlight(wy == 2017) +
+  ggtitle("American River at Fair Oaks, 11446500" )
+
+
+# stage
+start.date <- "2007-10-01"
+end.date <- "2023-09-30"
+pCode <- "00065" #00065 is stage, 00060 is flow
+amrcn_fo <- readNWISuv(siteNumbers = siteNo,
+                       parameterCd = pCode,
+                       startDate = start.date,
+                       endDate = end.date)
+
+amrcn_fo <- renameNWISColumns(amrcn_fo)
+names(amrcn_fo)
+
+df <- amrcn_fo %>% mutate(wy= water_year(dateTime))
+head(df)
+
+df_annpeak <- df %>% group_by(wy) %>% summarize(peak = max(GH_Inst)) %>% arrange(desc(peak))
+df_annpeak
+write_csv(df_annpeak, "amrcn_fairoaks_wy_instantaneous_peakstage_ft.txt")
